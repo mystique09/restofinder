c3f41edc8e319dbe728aff8ea3e1afc091acb803
mystique09 <knightkrypto7@gmail.com>
2025-07-19 03:26:50 +0800

    add: try/catch

--- src/application/usecases/find-restaurant-usecase.ts
+++ src/application/usecases/find-restaurant-usecase.ts
@@ -7,7 +7,7 @@ import type {
 import type { FourSquarePlaceSearchResponse } from "../../domain/models/restaurant.js";
 
 export class FourSquarePlaceSearcUsecase {
-  constructor(private llmProvider: LLMProvider) {}
+  constructor(private llmProvider: LLMProvider) { }
 
   async execute(
     search: string
@@ -16,27 +16,31 @@ export class FourSquarePlaceSearcUsecase {
       return { error: "Query must not be empty" };
     }
 
-    const geminiResponse = (await this.llmProvider.completion(
-      search
-    )) as GeminiResponse;
-    const { candidates } = geminiResponse;
-    const parts =
-      candidates.map((c) => c.content.parts).find((c) => c.length > 0) ?? [];
+    try {
+      const geminiResponse = (await this.llmProvider.completion(
+        search
+      )) as GeminiResponse;
+      const { candidates } = geminiResponse;
+      const parts =
+        candidates.map((c) => c.content.parts).find((c) => c.length > 0) ?? [];
 
-    const functionCall = parts
-      .map((p) => p.functionCall)
-      .find((p) => p.name === "placeSearch");
+      const functionCall = parts
+        .map((p) => p.functionCall)
+        .find((p) => p.name === "placeSearch");
 
-    if (!functionCall) {
+      if (!functionCall) {
+        return { error: "Invalid response from LLM provider" };
+      }
+
+      const toolName = functionCall.name;
+      const tool = this.llmProvider.getTool(toolName);
+
+      const functionCallArgs = functionCall.args as SearchPlace;
+      const response = await tool?.execute(functionCallArgs);
+
+      return { ok: response };
+    } catch (e) {
       return { error: "Invalid response from LLM provider" };
     }
-
-    const toolName = functionCall.name;
-    const tool = this.llmProvider.getTool(toolName);
-
-    const functionCallArgs = functionCall.args as SearchPlace;
-    const response = await tool?.execute(functionCallArgs);
-
-    return { ok: response };
   }
 }
